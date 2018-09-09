bdfc44644ac1985d8f5eb567655e2704f8bfb4c5
Roy Doyal <rldoyal@hotmail.com>
2018-09-08 17:36:33 -0700

    Working version -- i think :)

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -5,12 +5,25 @@ using System.Threading;
 namespace eCommerce
 {
     public delegate void priceCutEvent(int pr);
+    public delegate void orderProcessedEvent(Int32 SenderID, Int32 amountToChage, Int32 price, Int32 amount);
+    public delegate void orderCreatedEvent();
 
     public class OrderObject
     {
         Int32 senderId; // ID of thread
         long  cardNo;  // credit card info
         Int32 amount; // number of chickens to order
+
+        public OrderObject()
+        {
+
+        }
+        public OrderObject( Int32 Id, long CNum, Int32 Amt)
+        {
+            senderId = Id;
+            cardNo = CNum;
+            amount = Amt;
+        }
         public void setID( Int32 id )
         {
             senderId = id;
@@ -31,77 +44,42 @@ namespace eCommerce
 
     }
 
-    public class BufferObject
-    {
-
-        private OrderObject orderObj;
-        private bool writeable = true;
-
-        public void setOrderObject(OrderObject order)
-        {
-            while (!writeable)
-            {
-                try
-                {
-                    Monitor.Wait(this);
-                }
-                catch { Console.WriteLine(" error in setOrderObject"); }
-            }
-            orderObj = order;
-            writeable = true;
-            Monitor.PulseAll(this);
-        }
-
-        public OrderObject getOrderObject()
-        {
-            while (writeable)
-            {
-                try
-                {
-                    Monitor.Wait(this);
-                }
-                catch { Console.WriteLine(" error in getOrderObject"); }
-            }
-            writeable = true;
-            Monitor.PulseAll(this);
-            return orderObj;
-        }
-    }
+    
 
     public class BufferString
     {
         private string bufferStr;
 
         private bool writeable = true;
-
+        public BufferString()
+        {
+            writeable = true;
+            bufferStr = String.Empty;
+        }
         public void setBufferString(string str)
         {
-            while (!writeable)
-            {
-                try
-                {
-                    Monitor.Wait(this);
-                }
-                catch { Console.WriteLine(" error in setOrderObject"); }
-            }
+           
             bufferStr = str;
-            writeable = true;
-            Monitor.PulseAll(this);
+            writeable = false;  // has data
+          
         }
 
         public string getBufferString()
         {
-            while (writeable)
-            {
-                try
-                {
-                    Monitor.Wait(this);
-                }
-                catch { Console.WriteLine(" error in getOrderObject"); }
-            }
-            writeable = true;
-            Monitor.PulseAll(this);
+         
+            writeable = true; // data read
             return bufferStr;
+        }
+
+        public bool isWriteable()
+        {
+            return writeable;
+        }
+
+        public void Reset()  // reset the values
+        {
+            writeable = true;
+            bufferStr = String.Empty;
         }
     }
      
@@ -109,7 +87,7 @@ namespace eCommerce
     {
         // encoder takes an object and returns a string to the retailer
         // create a CSV string
-        public BufferString Encoder( OrderObject myObj)
+        public static BufferString Encoder( OrderObject myObj)
         {
           
             BufferString myBS = new BufferString();
@@ -126,7 +104,7 @@ namespace eCommerce
         }
 
         // decoder takes a string and returns a object
-        public OrderObject decoder( BufferString myStr)
+        public static OrderObject Decoder( BufferString myStr)
         {
             OrderObject myObj = new OrderObject();
             // split the string
@@ -142,15 +120,15 @@ namespace eCommerce
 
     public class MultiCellBuffer
     {
-        public String[] buffers;
-        private const int N = 2;
+        public BufferString[] buffers;
+        private const int N = 5;
         private int n; // number of cells
         private int elementCount;
         private static Semaphore write_pool;
         private static Semaphore read_pool;
 
         // constructor for class
-        public void MulitCellBuffer(int n)
+        public MultiCellBuffer(int n)
         {
             lock (this) // we want no interruptiongs
             {
@@ -161,11 +139,13 @@ namespace eCommerce
                     this.n = n;
                     write_pool = new Semaphore(n, n);
                     read_pool = new Semaphore(n, n);
-                    buffers = new String[n];
+                    buffers = new BufferString[3];
+                    
 
                     for (int i = 0; i < n; i++)
                     {
-                        buffers[i] = String.Empty;
+                        buffers[i] = new BufferString();
+                 
                     }
                 }
                 else
@@ -173,7 +153,7 @@ namespace eCommerce
             }
         }
          
-        public void setOneCell(String data)
+        public void setOneCell(BufferString data)
         {
             write_pool.WaitOne();
 
@@ -186,7 +166,7 @@ namespace eCommerce
 
                 for (int i = 0; i < n; i++)
                 {
-                    if (buffers[i].()) // make sure empty
+                    if (buffers[i].isWriteable()) // make sure empty
                     {
                         buffers[i] = data;
                         elementCount++;
@@ -198,9 +178,9 @@ namespace eCommerce
             }
         }
 
-        public String getOneCell()
+        public BufferString getOneCell()
         {
-            string outStr = String.Empty;
+            BufferString outStr = null;
             read_pool.WaitOne();
 
             lock (this)
@@ -212,14 +192,16 @@ namespace eCommerce
                 
                 for (int i = 0; i < n; i++)
                 {
-                   
-                    if (!string.IsNullOrEmpty(buffers[i])) // make sure cell has data
+
+                    if (!buffers[i].isWriteable()) // make sure cell has data
                     {
-                        outStr = buffers[i];
-                        buffers[i] = String.Empty;
+                        outStr = new BufferString();
+                        outStr.setBufferString(buffers[i].getBufferString());
+                        buffers[i].Reset();
                         elementCount--;
                         i = n;
                     }
+                               
                 }
                 read_pool.Release();
                 Monitor.Pulse(this);
@@ -232,13 +214,13 @@ namespace eCommerce
     public class Orderprocessing
     {
         public static event orderProcessedEvent orderProcessed; // event triggered whe a new order has beeen processed
-        public static void processrOrder( OrderObject orderObj, Int32 unitPrice )
+        public static void processOrder( OrderObject orderObj, Int32 unitPrice )
         {
             long cardNo = orderObj.getCardNo();
             if ( (cardNo <= 9999 && cardNo >= 9000))
             {
                 Int32 paymentAmount = Convert.ToInt32(1.10 * (unitPrice * orderObj.getAmt()));  // price * quanity + tax(10%)
-                orderProcessed(orderObj.getID, paymentAmount, unitPrice, orderObj.getAmt());
+                orderProcessed(orderObj.getID(), paymentAmount, unitPrice, orderObj.getAmt());
             }
             else
             {
@@ -250,47 +232,123 @@ namespace eCommerce
 
     public class ChickenFarm
     {
-        static Random rng = new Random();
+        static Random rnd = new Random();
         public static event priceCutEvent PriceCut;
-        private static Int32 chickenPrice = 10;
-        public Int32 getPrice() { return chickenPrice; }
 
+        private static Int32 priceCutCount = 0;
+        private static Int32 loc = 0;
+        private static Int32 chickenPrice = 35;
+        private static Int32 priceUpdateCounter = 0; // when this gets to 10, then up the price and reset
+
+        public Int32 getPrice() { return chickenPrice; }
+        
         public static void changePrice(Int32 price)
+
         {
-            if (price < chickenPrice)
+            //Int32 Len =  myApplication.retailers.Length;
+            //if (loc == myApplication.retailers.Length)
+            //        loc = 0;
+
+            if (PriceCut != null)
             {
-                // price cut
-                if (PriceCut != null)
-                    PriceCut(price);
+                if (price < chickenPrice)
+                {
+                    // price cut
+                    if (PriceCut != null)
+                        if (price < chickenPrice)
+                        {
+                            PriceCut(price);
+                            loc++;
+                            priceCutCount++;
+                        }
+                    if (price != chickenPrice)
+                        chickenPrice = price;
+
+                }
             }
-            chickenPrice = price;
+            
         }
         public void farmerFunc()
         {
-            for (Int32 i = 0; i <50; i++)
+            while (priceCutCount < 10)
             {
-                Thread.Sleep(500);
-                //take the order from the queue of the orders;
-                // decide the price based on teh orders
-                Int32 p = rng.Next(5, 10);
-                Console.WriteLine("New Price is {0}", p);
-                ChickenFarm.changePrice(p);
+                Thread.Sleep(rnd.Next(1000, 2000));
+                Int32 price = pricingModel();
+                changePrice(price);
             }
+            myApplication.chickenThreadRunning = false;
+        }
+
+        private  Int32 pricingModel()
+        {
+            Int32 price = 0;
+            priceUpdateCounter++;
+
+            if (priceUpdateCounter >= 10)
+            {
+                price = 40;// max the price
+                priceUpdateCounter = 0;
+            }
+            else
+            {
+                price = rnd.Next(10, 60);
+            }
+            return price;
+        }
+       
+        public void runOrder() // event handler
+        {
+            BufferString order = myApplication.mcb.getOneCell();
+            OrderObject orderObj = EncodeDecode.Decoder(order);
+            Thread thread = new Thread(() => Orderprocessing.processOrder(orderObj, getPrice()));
+            thread.Start();
         }
     }
 
+
     public class Retailer
     {
+    
+        public static event orderCreatedEvent orderCreated;
+        public static Random rnd = new Random();
+
+
         public void retailerFunc()
         {
-            ChickenFarm chicken = new ChickenFarm();
-            for (Int32 i = 0; i < 10; i++)
+         
+            while (myApplication.chickenThreadRunning)
             {
-                Thread.Sleep(1000);
-                Int32 p = chicken.getPrice();
-                Console.WriteLine("Store{0} has everyday low prices: ${1} each", Thread.CurrentThread.Name, p);
+                Thread.Sleep(rnd.Next(1500, 3000));
+                createorder(Int32.Parse(Thread.CurrentThread.Name));
             }
         }
+
+        private void createorder(Int32 senderID)
+        {
+            long cardNo = rnd.Next(9000, 9999);
+            Int32 amount = rnd.Next(10, 1000);
+
+            OrderObject orderObj = new OrderObject(senderID, cardNo, amount);
+            BufferString orderString = new BufferString();
+            orderString = EncodeDecode.Encoder(orderObj);
+
+            Console.WriteLine(" Store {0}'s order was created at {1}.", senderID.ToString(), DateTime.Now.ToString("hh:mm:ss"));
+
+            myApplication.mcb.setOneCell(orderString);
+            orderCreated();
+           
+        }
+
+        public void orderProcessed(Int32 senderID, Int32 amountToCharge, Int32 price, Int32 amount)
+        {
+
+            Console.WriteLine(" Store {0}'s order has been processed.  amount to be chaged is ${1} = ${2} (price) * {3} (amount) of chickens",
+                 senderID.ToString(),  amountToCharge, price, amount);
+            Console.WriteLine("           time : {0}", DateTime.Now.ToString("hh:mm:ss"));
+
+        }
+
+          
         public void chickenOnSale( Int32 p)
         {
             //order chickens from farm.
@@ -299,15 +357,28 @@ namespace eCommerce
     }
     public class myApplication
     {
+        public static bool chickenThreadRunning = true;
+        public static MultiCellBuffer mcb;
+        public static Thread[] retailers;
+
+
         static void Main(string[] args)
         {
             ChickenFarm chicken = new ChickenFarm();
+            Retailer chickenstore = new Retailer();
+
+            mcb = new MultiCellBuffer(3);
+
+            
             Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
             farmer.Start();
-            Retailer chickenstore = new Retailer();
+           
             ChickenFarm.PriceCut += new priceCutEvent(chickenstore.chickenOnSale);
-            Thread[] retailers = new Thread[3];
-            for (Int32 i = 0; i < 3; i++)
+            Retailer.orderCreated += new orderCreatedEvent(chicken.runOrder);
+            Orderprocessing.orderProcessed += new orderProcessedEvent(chickenstore.orderProcessed);
+
+            Thread[] retailers = new Thread[5];
+            for (Int32 i = 0; i < 5; i++)
             {
                 retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
                 retailers[i].Name = (i + 1).ToString();
